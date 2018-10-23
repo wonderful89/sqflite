de050fbdb66e8a069967dfda367baab85014eff6
qianzhao <qianzhao@tencent.com>
2018-10-23 18:27:19 +0800

    update.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 
 #echo "############################## begin build"
 #echo "############################## end build"
---
+++ lib/mytests/enum_one.build.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'enum_one.dart';
+
+// **************************************************************************
+// EnumGenerator
+// **************************************************************************
+
+EnumTypeOne buildEnumTypeOne(int index) {
+  try {
+    return EnumTypeOne.values[index];
+  } catch (e) {
+    return null;
+  }
+}
